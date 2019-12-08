79c73552ce2b47dbf1fa80961c38c83d7ff0ec5a
kaisa316 <490788241@qq.com>
2019-12-08 22:14:46 +0800

    选择排序

--- sort.php
+++ sort.php
@@ -8,7 +8,7 @@ function select_sort_find_min($input_arr) {
 	$min_index = null;
 	$min = null;
 	foreach($input_arr as $index=>$val) {
-		if(!isset($min_index) ) {
+		if(is_null($min_index) ) {
 			$min_index = $index;
 			$min = $val;
 		}
@@ -33,11 +33,14 @@ function select_sort_main() {
 	$sorted_arr = [];
 	for($i=0;$i<$len;$i++) {
 		$min = select_sort_find_min($arr);	
+		//1. 一遍遍历能够找出其中的最小值
+		//2.将最小值放到新数组
+		//3、原数组的最小值去除，不再参与新一轮的排序。
 		$sorted_arr[] = $min['val']; 
 		unset($arr[$min['index']]);
 	}
 	print_r($sorted_arr);
 }
 
-main();
+select_sort_main();
 
