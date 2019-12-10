dca52f8aebe125266add95a48ca20e0c47a9a9ba
kaisa316 <490788241@qq.com>
2019-12-10 08:31:12 +0800

    排序

--- recursive.php
+++ recursive.php
@@ -1,14 +1,38 @@
 <?php
+/**
+ * 【递归】
+ * 自己调用自己
+ * 不断的缩小范围，靠近基线条件
+ * 基线条件，即停止条件，应该非常简单才行
+ *
+ *
+ * 由简入繁
+ * 从0或者1开始，找出其中的规律，从最简单的情况入手，从第一元素入手
+ * 程序中，有call stack
+ */
 
-function fn($x) {
-	if($x == 1) {
+
+/**
+ * 计算 f(5)=5*4*3*2*1 
+ */
+function fn($n) {
+	//debug_print_backtrace();
+	if($n == 1) {
 		return 1;
 	}
-	return $x*fn($x-1);
+	return  $n*fn($n-1);
+}
+
+/**
+ * 用方块平均分割一块土地。找出最大的方块
+ */
+function max_squre() {
+	$side = 1680;
+	$other_side = 640;
 }
 
 function main() {
-	echo fn(3);
+	echo fn(5);
 }
 
 main();
