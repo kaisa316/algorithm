6159894175281cd411a64d4ad6d9184182ca3800
kaisa316 <490788241@qq.com>
2019-12-28 22:10:41 +0800

    贪婪算法-集合覆盖问题

--- greedy.php
+++ greedy.php
@@ -14,7 +14,7 @@
  * 所以每次都找结束最早的那堂课 
  * 下堂课的开始时间必须在堂课的结束时间之后的
  */
-function greedy_class() {
+function greedy_classroom() {
     //所有的课程
     $all_course = [
         ['name' => '美术课', 'start_time' => 9, 'end_time' => 10],
@@ -84,4 +84,76 @@ function next_course($all_course, $pre_end_time) {
     return $result;
 }
 
-greedy_class();
+/**
+ * 贪婪算法--集合覆盖问题
+ * 希望用最少的电台覆盖最多的州
+ * 而电台之间覆盖的州可能会重复
+ * 解题思路：每次找覆盖最多[未覆盖州】的电台
+ */
+function greedy_collection() {
+    //电台与城市的映射
+    $radio_city_map = [
+        'A'=>[
+            '北京','上海',
+        ],
+        'B'=>[
+            '深圳','上海','广东',
+        ],
+        'C'=>[
+            '北京','内蒙','河北',
+        ],
+        'D'=>[
+            '河北','山东','山西','陕西','宁夏',
+        ],
+        'E'=>[
+            '深圳','上海'
+        ],
+    ]; 
+    //所有的城市
+    $all_citys = [
+        '北京','上海','深圳','广东','内蒙','河北','山东','山西','陕西','宁夏',
+    ];
+    //未覆盖的城市
+    $uncover_citys = $all_citys;//初始值都未覆盖
+    $result_radio = [];//最终的结果集 
+    do {
+        $radio = get_radio_station($uncover_citys, $radio_city_map,$result_radio);
+        if(empty($radio)) {
+            break;
+        }
+        array_push($result_radio, $radio);
+        //更新未覆盖的结果集
+        $uncover_citys = array_diff($uncover_citys,$radio_city_map[$radio]);
+        //删除已覆盖的电台
+        unset($radio_city_map[$radio]);
+    } while(!empty($radio));
+
+    print_r($result_radio);
+}
+
+/**
+ * 获取覆盖最多【未覆盖的州】的电台
+ * @param uncover_citys 未覆盖的城市
+ * @param radio_city_map 电台与城市的映射
+ * @return radio key
+ */
+function get_radio_station($uncover_citys,$radio_city_map) {
+    $radio_key = null;
+    $max_uncover_num = null;
+    foreach($radio_city_map as $key=>$val) {
+        $intersect = array_intersect($uncover_citys,$val);//与未覆盖的州取交集
+        $intersect_cnt = count($intersect);
+        if($intersect_cnt == 0) {
+            continue;//无交集，证明都已经被覆盖了
+        }
+        if(is_null($radio_key) ||  $intersect_cnt > $max_uncover_num) {
+            $radio_key = $key;
+            $max_uncover_num = $intersect_cnt;
+        }
+    }
+
+    return $radio_key;
+}
+
+// greedy_classroom();
+greedy_collection();
